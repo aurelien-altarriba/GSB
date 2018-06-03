d021d7329845158226e2bceef55e1e8062aff09b
Aurélien <aurelien.altarriba@gmail.com>
2018-06-03 14:55:31 +0200

    Codage de l'onglet Personnel

--- GSB/Form1.cs
+++ GSB/Form1.cs
@@ -12,6 +12,14 @@ namespace GSB
 {
     public partial class Interface : System.Windows.Forms.Form
     {
+        Personnel unPersonnel;
+        List<Personnel> lesPersonnels;
+        Visiteur unVisiteur;
+        List<Visiteur> lesVisiteurs;
+        Technicien unTechnicien;
+        List<Technicien> lesTechniciens;
+
+
         public Interface()
         {
             InitializeComponent();
@@ -110,6 +118,199 @@ namespace GSB
         private void visite_Click(object sender, EventArgs e)
         {
 
+        }
+
+        // ******************************************************* //
+
+        private void Interface_Load(object sender, EventArgs e)
+        {
+            lesPersonnels = new List<Personnel>();
+            lesVisiteurs = new List<Visiteur>();
+            lesTechniciens = new List<Technicien>();
+        }
+
+        public void actualiserListePersonnelOngletPersonnel()
+        {
+            listePersonnel.Items.Clear();
+
+            foreach (Personnel Per in lesPersonnels)
+            {
+                listePersonnel.Items.Add(Per.infos());
+            }
+
+            foreach (Visiteur Vis in lesVisiteurs)
+            {
+                listePersonnel.Items.Add(Vis.infos());
+            }
+
+            foreach (Technicien Tec in lesTechniciens)
+            {
+                listePersonnel.Items.Add(Tec.infos());
+            }
+        }
+
+        private void btAjouterPersonnel_Click(object sender, EventArgs e)
+        {
+            if (rbVisiteur.Checked || rbDéléguéRégional.Checked)
+            {
+                unVisiteur = new Visiteur(tbObjectifVisiteur.Text, Convert.ToInt32(tbPrimeVisiteur.Text), tbAvantagesVisiteur.Text, Convert.ToInt32(tbBudgetVisiteur.Text), tbNomPersonnel.Text, tbPrénomPersonnel.Text, tbDateEmbauchePersonnel.Text, tbRégionPersonnel.Text, tbMailPersonnel.Text);
+                lesVisiteurs.Add(unVisiteur);
+            }
+
+            else if (rbTechnicien.Checked || rbTechnicienSupérieur.Checked)
+            {
+                unTechnicien = new Technicien(Convert.ToInt32(tbNiveauInterventionTechnicien.Text), tbFormationTechnicien.Text, tbCompetencesTechnicien.Text, tbNomPersonnel.Text, tbPrénomPersonnel.Text, tbDateEmbauchePersonnel.Text, tbRégionPersonnel.Text, tbMailPersonnel.Text);
+                lesTechniciens.Add(unTechnicien);
+            }
+
+            else
+            {
+                unPersonnel = new Personnel(tbNomPersonnel.Text, tbPrénomPersonnel.Text, tbDateEmbauchePersonnel.Text, tbRégionPersonnel.Text, tbMailPersonnel.Text);
+                lesPersonnels.Add(unPersonnel);
+            }
+
+            actualiserListePersonnelOngletPersonnel();
+        }
+
+        private void btModifierPersonnel_Click(object sender, EventArgs e)
+        {
+            bool trouvé = false;
+
+            foreach (Personnel Per in lesPersonnels)
+            {
+                // Si l'ID correspond à celui trouvé dans la liste lesPersonnels
+                if (Per.Id_personnel == Convert.ToInt16(tbIDPersonnelModifier.Text))
+                {
+                    Per.Nom = tbNomPersonnel.Text;
+                    Per.Prenom = tbPrénomPersonnel.Text;
+                    Per.Date_embauche = tbDateEmbauchePersonnel.Text;
+                    Per.Region_carriere = Per.Region_carriere + " - " + tbRégionPersonnel.Text;
+                    Per.Mail = tbMailPersonnel.Text + "@swiss-galaxy.com";
+
+                    trouvé = true;
+                }
+            }
+
+            if (trouvé == false)
+            {
+                foreach (Visiteur Vis in lesVisiteurs)
+                {
+                    if (Vis.Id_personnel == Convert.ToInt16(tbIDPersonnelModifier.Text))
+                    {
+                        Vis.Nom = tbNomPersonnel.Text;
+                        Vis.Prenom = tbPrénomPersonnel.Text;
+                        Vis.Date_embauche = tbDateEmbauchePersonnel.Text;
+                        Vis.Region_carriere = Vis.Region_carriere + " - " + tbRégionPersonnel.Text;
+                        Vis.Mail = tbMailPersonnel.Text + "@swiss-galaxy.com";
+                        Vis.Objectif = tbObjectifVisiteur.Text;
+                        Vis.Prime = Convert.ToInt32(tbPrimeVisiteur.Text);
+                        Vis.Avantages = tbAvantagesVisiteur.Text;
+                        Vis.Budget = Convert.ToInt32(tbBudgetVisiteur.Text);
+
+                        trouvé = true;
+                    }
+                }
+            }
+
+            if (trouvé == false)
+            {
+                foreach (Technicien Tec in lesTechniciens)
+                {
+                    if (Tec.Id_personnel == Convert.ToInt16(tbIDPersonnelModifier.Text))
+                    {
+                        Tec.Nom = tbNomPersonnel.Text;
+                        Tec.Prenom = tbPrénomPersonnel.Text;
+                        Tec.Date_embauche = tbDateEmbauchePersonnel.Text;
+                        Tec.Region_carriere = Tec.Region_carriere + " - " + tbRégionPersonnel.Text;
+                        Tec.Mail = tbMailPersonnel.Text + "@swiss-galaxy.com";
+                        Tec.Niveau_intervention = Convert.ToInt32(tbNiveauInterventionTechnicien.Text);
+                        Tec.Formation = tbFormationTechnicien.Text;
+                        Tec.Competences = tbCompetencesTechnicien.Text;
+
+                        trouvé = true;
+                    }
+                }
+            }
+
+            if (trouvé == false)
+            {
+                MessageBox.Show("Aucun personnel avec l'ID " + tbIDPersonnelModifier.Text + " n'a été trouvé");
+            }
+            else
+            {
+                actualiserListePersonnelOngletPersonnel();
+
+                MessageBox.Show("Personnel " + tbIDPersonnelModifier + " modifié avec succés");
+            }
+        }
+
+        private void btSupprimerPersonnel_Click(object sender, EventArgs e)
+        {
+            bool trouvé = false;
+            Personnel PersonnelSupprimer = null;
+            Visiteur VisiteurSupprimer = null;
+            Technicien TechnicienSupprimer = null;
+
+            foreach (Personnel Per in lesPersonnels)
+            {
+                // Si l'ID correspond à celui trouvé dans la liste lesPersonnels
+                if (Per.Id_personnel == Convert.ToInt16(tbIDPersonnelSupprimer.Text))
+                {
+                    PersonnelSupprimer = Per;
+
+                    trouvé = true;
+                }
+            }
+
+            if (trouvé)
+            {
+                lesPersonnels.Remove(PersonnelSupprimer);
+            }
+            else
+            {
+                foreach (Visiteur Vis in lesVisiteurs)
+                {
+                    if (Vis.Id_personnel == Convert.ToInt16(tbIDPersonnelSupprimer.Text))
+                    {
+                        VisiteurSupprimer = Vis;
+
+                        trouvé = true;
+                    }
+                }
+
+                if (trouvé)
+                {
+                    lesVisiteurs.Remove(VisiteurSupprimer);
+                }
+                else
+                {
+                    foreach (Technicien Tec in lesTechniciens)
+                    {
+                        if (Tec.Id_personnel == Convert.ToInt16(tbIDPersonnelSupprimer.Text))
+                        {
+                            TechnicienSupprimer = Tec;
+
+                            trouvé = true;
+                        }
+                    }
+
+                    if (trouvé)
+                    {
+                        lesTechniciens.Remove(TechnicienSupprimer);
+                    }
+                }
+            }
+
+            if (trouvé == false)
+            {
+                MessageBox.Show("Aucun personnel avec l'ID " + tbIDPersonnelSupprimer.Text + " n'a été trouvé");
+            }
+            else
+            {
+                actualiserListePersonnelOngletPersonnel();
+
+                MessageBox.Show("Personnel " + tbIDPersonnelSupprimer + " supprimé avec succés");
+            }
         }
     }
 }
---
+++ GSB/Personnel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSB
+{
+    class Personnel
+    {
+        int id_personnel;
+        string nom;
+        string prenom;
+        string matricule;
+        string date_embauche;
+        string region_carriere;
+        string mail;
+
+        static int num_id_personnel = 0;
+
+        public Personnel(string unNom, string unPrenom, string uneDate_embauche, string uneRegion_carriere, string unMail)
+        {
+            num_id_personnel = num_id_personnel + 1;
+            id_personnel = num_id_personnel;
+            nom = unNom;
+            prenom = unPrenom;
+            matricule = "A GENERER";
+            date_embauche = uneDate_embauche;
+            region_carriere = region_carriere + " - " + uneRegion_carriere;
+            mail = unMail + "@swiss-galaxy.com";
+        }
+
+        public string infos()
+        {
+            return id_personnel + " : " + nom + " " + prenom + " (" + matricule + ") - " + mail + " : Embauché le " + date_embauche + " à: " + region_carriere;
+        }
+
+        public int Id_personnel
+        {
+            get
+            {
+                return id_personnel;
+            }
+
+            set
+            {
+                id_personnel = value;
+            }
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return prenom;
+            }
+
+            set
+            {
+                prenom = value;
+            }
+        }
+
+        public string Matricule
+        {
+            get
+            {
+                return matricule;
+            }
+
+            set
+            {
+                matricule = value;
+            }
+        }
+
+        public string Date_embauche
+        {
+            get
+            {
+                return date_embauche;
+            }
+
+            set
+            {
+                date_embauche = value;
+            }
+        }
+
+        public string Region_carriere
+        {
+            get
+            {
+                return region_carriere;
+            }
+
+            set
+            {
+                region_carriere = value;
+            }
+        }
+
+        public string Mail
+        {
+            get
+            {
+                return mail;
+            }
+
+            set
+            {
+                mail = value;
+            }
+        }
+    }
+}
